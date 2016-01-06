66bb802d904118709ae6049a674c5ba1148d9f9c
Ben Jakuben <ben.jakuben@gmail.com>
2016-01-06 08:11:27 -0800

    Refactored Palette stuff to be synchronous

--- app/src/main/java/com/teamtreehouse/albumcover/AlbumDetailActivity.java
+++ app/src/main/java/com/teamtreehouse/albumcover/AlbumDetailActivity.java
@@ -43,36 +43,37 @@ public class AlbumDetailActivity extends Activity {
         int albumArtResId = getIntent().getIntExtra(EXTRA_ALBUM_ART_RESID, R.drawable.mean_something_kinder_than_wolves);
         albumArtView.setImageResource(albumArtResId);
 
-        // Reduce bitmap size
+        Bitmap albumBitmap = getReducedBitmap(albumArtResId);
+        colorizeFromImage(albumBitmap);
+    }
+
+    private Bitmap getReducedBitmap(int albumArtResId) {
+        // reduce image size in memory to avoid memory errors
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = false;
         options.inSampleSize = 8;
-
-        // asynchronously generate panel colors from Palette
-        Bitmap albumBitmap = BitmapFactory.decodeResource(getResources(), albumArtResId, options);
-        if (albumBitmap != null && !albumBitmap.isRecycled()) {
-            new Palette.Builder(albumBitmap).generate(paletteListener);
-        }
+        return BitmapFactory.decodeResource(getResources(), albumArtResId, options);
     }
 
-    Palette.PaletteAsyncListener paletteListener = new Palette.PaletteAsyncListener() {
-        public void onGenerated(Palette palette) {
-            // access palette colors here
-            int defaultPanelColor = 0xFF808080;
-            int defaultFabColor = 0xFFEEEEEE;
-            titlePanel.setBackgroundColor(palette.getDarkVibrantColor(defaultPanelColor));
-            trackPanel.setBackgroundColor(palette.getLightMutedColor(defaultPanelColor));
+    private void colorizeFromImage(Bitmap image) {
+        Palette palette = Palette.from(image).generate();
 
-            int[][] states = new int[][] {
-                    new int[] { android.R.attr.state_enabled },
-                    new int[] { android.R.attr.state_pressed }
-            };
+        // set panel colors
+        int defaultPanelColor = 0xFF808080;
+        int defaultFabColor = 0xFFEEEEEE;
+        titlePanel.setBackgroundColor(palette.getDarkVibrantColor(defaultPanelColor));
+        trackPanel.setBackgroundColor(palette.getLightMutedColor(defaultPanelColor));
 
-            int[] colors = new int[] {
-                    palette.getVibrantColor(defaultFabColor),
-                    palette.getLightVibrantColor(defaultFabColor)
-            };
-            fab.setBackgroundTintList(new ColorStateList(states, colors));
-        }
-    };
+        // set fab colors
+        int[][] states = new int[][]{
+                new int[]{android.R.attr.state_enabled},
+                new int[]{android.R.attr.state_pressed}
+        };
+
+        int[] colors = new int[]{
+                palette.getVibrantColor(defaultFabColor),
+                palette.getLightVibrantColor(defaultFabColor)
+        };
+        fab.setBackgroundTintList(new ColorStateList(states, colors));
+    }
 }
